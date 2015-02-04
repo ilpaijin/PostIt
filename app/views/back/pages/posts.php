3cbda451ef768f07fc5e02afb0653ef201b5579d
Ilpaijin <ilpaijin@gmail.com>
2015-02-04 08:52:40 +0100

    admin posts list fix

--- app/views/back/pages/posts.php
+++ app/views/back/pages/posts.php
@@ -1,38 +1,43 @@
 <h1 class="page-header">Posts</h1>
 <h3>Posts list</h3>
-<div class="table-responsive">
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Status</th>
-                <th>Author</th>
-                <th>Title</th>
-                <th>Created</th>
-            </tr>
-        </thead>
-        <tbody>
-            {% for post in posts %}
 
+{% if posts %}
+
+    <div class="table-responsive">
+        <table class="table table-striped">
+            <thead>
                 <tr>
-                    <td>
-                        {{post.id}}
-                    </td>
-                    <td>
-                        {% if(date(post.published) < date('now')) %}
-                            <span class="label label-success">Online</span>
-                        {% else %}
-                            <span class="label label-warning">Pending</span>
-                        {% endif %}
-                    </td>
-                    <td>{{ post.author}}</td>
-                    <td>{{ post.title}}</td>
-                    <td>{{ post.date_created}}</td>
+                    <th>#</th>
+                    <th>Status</th>
+                    <th>Author</th>
+                    <th>Title</th>
+                    <th>Created</th>
                 </tr>
-            {% endfor %}
-        </tbody>
-    </table>
-</div>
+            </thead>
+            <tbody>
+                {% for post in posts %}
+
+                    <tr>
+                        <td>
+                            {{post.id}}
+                        </td>
+                        <td>
+                            {% if(date(post.published) < date('now')) %}
+                                <span class="label label-success">Online</span>
+                            {% else %}
+                                <span class="label label-warning">Pending</span>
+                            {% endif %}
+                        </td>
+                        <td>{{ post.author}}</td>
+                        <td>{{ post.title}}</td>
+                        <td>{{ post.date_created}}</td>
+                    </tr>
+                {% endfor %}
+            </tbody>
+        </table>
+    </div>
+    
+{% endif %}
 
 <hr>
 <h3>Post it now!</h3>
