b8a5e5af3eade899eb9669dac2f6f4ca9e434d39
Ilpaijin <ilpaijin@gmail.com>
2015-02-13 10:12:46 +0100

    controller docblockr

--- src/PostIt/Application/Controllers/Controller.php
+++ src/PostIt/Application/Controllers/Controller.php
@@ -16,13 +16,31 @@ use Symfony\Component\HttpFoundation\Response;
  */
 abstract class Controller
 {
+    /**
+     * Service Container
+     * 
+     * @var PostIt\Application\Contracts\Containerable
+     */
     protected $container;
 
+    /**
+     * [__construct description]
+     *
+     * @param Containerable $container
+     */
     public function __construct(Containerable $container)
     {
         $this->container = $container;
     }
 
+    /**
+     * Proxy to Twig render method and return a new Response.
+     *
+     * @param  string  $view
+     * @param  array   $parameters
+     * @param  integer $status
+     * @return Symfony\Component\HttpFoundation\Response
+     */
     public function render($view, $parameters = array(), $status = 200)
     {
         return new Response($this->container->get('twig')->render($view.'.php', $parameters), $status);
