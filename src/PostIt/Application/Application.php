14cf03adeefe64959adbd9b3cd4379870bc20fdd
Ilpaijin <ilpaijin@gmail.com>
2015-02-14 12:41:32 +0100

    method matcher splitted

--- src/PostIt/Application/Application.php
+++ src/PostIt/Application/Application.php
@@ -84,11 +84,9 @@ class Application
         $context = new RequestContext();
         $context->fromRequest($request);
 
-        $matcher = new UrlMatcher($this->routes, $context);
-
         try {
 
-            $matched = $matcher->match($request->getPathInfo());
+            $matched = $this->matchRequest($context);
 
             $request->attributes->add($matched, EXTR_SKIP);
 
@@ -98,17 +96,26 @@ class Application
 
             $controller = new $controller($this->container);
 
-            var_dump($this->containerGet('env'));
-
             return call_user_func(array($controller,$action), $request, $matched);
+
         } catch (ResourceNotFoundException $e) {
             //log error
-            // var_dump($e->getMessage());
             return call_user_func(array(new Controllers\ErrorController($this->container), 'notFound'), $request);
         } catch (Exception $e) {
             //log error
-            var_dump($e->getMessage());
             return call_user_func(array(new Controllers\ErrorController($this->container), 'server'), $request);
         }
+    }
+
+    /**
+     * Look for a match between the declared routes and the request
+     *
+     * @param Symfony\Component\Routing\RequestContext $context
+     */
+    private function matchRequest($context)
+    {
+        $matcher = new UrlMatcher($this->routes, $context);
+
+        return $matcher->match($context->getPathInfo());
     }
 }
