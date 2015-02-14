8920643531c6ffbc784c8cb9f47b1c0bdd47c9ef
Ilpaijin <ilpaijin@gmail.com>
2015-02-14 13:02:33 +0100

    refactoring app init

--- app/app.php
+++ app/app.php
@@ -3,60 +3,17 @@
 require __DIR__."/../vendor/autoload.php";
 require_once __DIR__.'/../vendor/twig/twig/lib/Twig/Autoloader.php';
 
-use PostIt\Application\Container;
-use PostIt\Application\Environment;
-use PostIt\Application\Config;
+$routes = require __DIR__.'/routes.php';
+
 use PostIt\Application\Session;
-use PostIt\Entities\User;
+use PostIt\Application\Container;
 
 date_default_timezone_set('Europe/Amsterdam');
 
 Session::start();
 
-$routes = require __DIR__.'/routes.php';
-
 $app = new PostIt\Application\Application(new PostIt\Application\Container(), $routes);
 
-require __DIR__.'/global.php';
-
-$env = Environment::detect();
-
-$app->containerSet('env', $env);
-$app->containerSet('template_path', __DIR__.'/views');
-$app->containerSet('config', new Config(__DIR__."/config/".$env.".json"));
-
-/*
-|--------------------------------------------------------------------------
-| Doctrine DBAL Service
-|--------------------------------------------------------------------------
-|
-*/
-$db = \Doctrine\DBAL\DriverManager::getConnection(
-    $app->containerGet('config')->get('db'),
-    new \Doctrine\DBAL\Configuration()
-);
-$app->containerSet('db', $db);
-
-/*
-|--------------------------------------------------------------------------
-| Twig Service
-|--------------------------------------------------------------------------
-|
-*/
-Twig_Autoloader::register();
-$loader = new Twig_Loader_Filesystem($app->containerGet('template_path'));
-$twig = new Twig_Environment($loader, array(
-    'cache' => __DIR__.'/views/cache',
-    'debug' => $app->containerGet('env')
-));
-$app->containerSet('twig', $twig);
-
-/*
-|--------------------------------------------------------------------------
-| USer Service
-|--------------------------------------------------------------------------
-|
-*/
-$app->containerSet('user', new User());
+require __DIR__.'/services.php';
 
 return $app;
---
+++ app/services.php
@@ -0,0 +1,88 @@
+<?php
+
+use Symfony\Component\Debug\ErrorHandler;
+use Symfony\Component\Debug\ExceptionHandler;
+use PostIt\Application\Environment;
+use PostIt\Application\Config;
+use PostIt\Entities\User;
+
+/*
+|--------------------------------------------------------------------------
+| Debug
+|--------------------------------------------------------------------------
+|
+*/
+$app->containerSet('debug', Environment::isDevelopment() ? true : false);
+
+/*
+|--------------------------------------------------------------------------
+| Errors/Exceptions handling
+|--------------------------------------------------------------------------
+|
+*/
+if ('cli' !== php_sapi_name()) {
+    ini_set('display_errors', 0);
+    ExceptionHandler::register($app->containerGet('debug'));
+} elseif (!ini_get('log_errors') || ini_get('error_log')) {
+    ini_set('display_errors', 1);
+}
+
+ErrorHandler::register();
+
+/*
+|--------------------------------------------------------------------------
+| Environment
+|--------------------------------------------------------------------------
+|
+*/
+$app->containerSet('env', Environment::detect());
+
+/*
+|--------------------------------------------------------------------------
+| Templating
+|--------------------------------------------------------------------------
+|
+*/
+$app->containerSet('template_path', __DIR__.'/views');
+
+/*
+|--------------------------------------------------------------------------
+| Config
+|--------------------------------------------------------------------------
+|
+*/
+$app->containerSet('config', new Config(__DIR__."/config/".Environment::detect().".json"));
+
+/*
+|--------------------------------------------------------------------------
+| Doctrine DBAL Service
+|--------------------------------------------------------------------------
+|
+*/
+$db = \Doctrine\DBAL\DriverManager::getConnection(
+$app->containerGet('config')->get('db'),
+new \Doctrine\DBAL\Configuration()
+);
+$app->containerSet('db', $db);
+
+/*
+|--------------------------------------------------------------------------
+| Twig Service
+|--------------------------------------------------------------------------
+|
+*/
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem($app->containerGet('template_path'));
+$twig = new Twig_Environment($loader, array(
+    'cache' => __DIR__.'/views/cache',
+    'debug' => $app->containerGet('env')
+));
+$app->containerSet('twig', $twig);
+
+/*
+|--------------------------------------------------------------------------
+| USer Service
+|--------------------------------------------------------------------------
+|
+*/
+$app->containerSet('user', new User());
